535d83408d25f56d7bdd3fc8f1762f0489fa6022
JacobDangCS50 <jacobohdang@gmail.com>
2022-08-22 17:09:44 -0700

    finished lab 3-4

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,49 +1,44 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js'
-import beastData from "../data.json"
 
 class Main extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      hbArray: beastData
-    };
-  }
-}
-
-render(){
-  return(
-    <div className="main">
-      {this.props.hornAmount == 'All' ? {
-        this.state.hbArray.map(beast => {
-          return (
-            <HornedBeast
-              key={beast._id}
-              title={beast.title}
-              img_url={beast.image_url}
-              description={beast.description}
-              handleClick={() => this.props.selectBeast(beast)}
+  render() {    
+    return (
+      <div className="main">
+        {this.props.hornAmount == 'All' ?
+          <>
+            {this.props.hbArray.map(beast => (
+              <HornedBeast
+                key={beast._id}
+                title={beast.title}
+                img_url={beast.image_url}
+                description={beast.description}
+                handleClick={() => this.props.selectBeast(beast)}
               />
-          )
-        });
-      }}
-      :
-      {let filteredArr = hbArray.filter (beast => beast.horns == this.props.hornChoice)
-          filteredArr(beast => {
-            return(
+            )
+            )
+            }
+          </>
+          :
+          <>
+          {
+          this.props.filteredArr.map(beast => {
+            return (
               <HornedBeast
                 key={beast._id}
                 title={beast.title}
                 img_url={beast.image_url}
                 description={beast.description}
                 handleClick={() => this.props.selectBeast}
-                />
+              />
             )
           })
+        }
+      </>
       }
     </div>
-  );
+)}
 }
 
 
